e5315f262e200b10954669b952a4abdb53e5cf31
Razvan Predescu <rzvdaniel@gmail.com>
2017-10-01 23:13:30 +0100

    Initial web project

---
+++ Cameleon.Web/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="home"> 
+  <a href="#" @click="currentView='Hello'">Hello</a>
+  <a href="#" @click="currentView='Bye'">Bye</a>
+  <component v-bind:is="currentView"></component>
+</div>
+</template>
+
+<script>
+
+var comps = {}
+
+comps['Hello'] = resolve => {
+  require.ensure(['./Hello.vue'], () => {
+    resolve(require('./Hello.vue'))
+  })
+}
+
+comps['Bye'] = resolve => {
+  require.ensure(['./Bye.vue'], () => {
+    resolve(require('./Bye.vue'))
+  })
+}
+
+export default {
+  name: 'home',
+  data: () => ({
+    currentView: 'Hello'
+  }),
+  components: comps
+}
+
+</script>
+
+<style>
+
+</style>
