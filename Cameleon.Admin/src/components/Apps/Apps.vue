39d7103a60eae9f38c24a32bb6fd74e6ebbad198
Razvan Predescu <rzvdaniel@gmail.com>
2017-12-22 09:21:15 +0000

    Added Apps table

--- Cameleon.Admin/src/components/Apps/Apps.vue
+++ Cameleon.Admin/src/components/Apps/Apps.vue
@@ -1,5 +1,38 @@
 <template>
 
+<div>
+
   <h1>The List of Apps</h1>
-  
+
+  <table id="mainTable" class="table table-striped m-b-0">
+    <thead>
+    <tr>
+        <th>Name</th><th>Cost</th><th>Profit</th><th>Fun</th>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td>Car</td><td>100</td><td>200</td><td>0</td>
+    </tr>
+    <tr>
+        <td>Bike</td><td>330</td><td>240</td><td>1</td>
+    </tr>
+    <tr>
+        <td>Plane</td><td>430</td><td>540</td><td>3</td>
+    </tr>
+    <tr>
+        <td>Yacht</td><td>100</td><td>200</td><td>0</td>
+    </tr>
+    <tr>
+        <td>Segway</td><td>330</td><td>240</td><td>1</td>
+    </tr>
+    </tbody>
+    <tfoot>
+    <tr>
+        <th><strong>TOTAL</strong></th><th></th><th></th><th></th>
+    </tr>
+    </tfoot>
+  </table>
+
+</div>
 </template>
