a2c76d303996375ffde3fb02af90250f31983e37
rzvdaniel <rzvdaniel@gmail.com>
2018-05-28 12:51:46 +0100

    Removed unnecessary Admin website

--- Cameleon.Admin/config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  WEBSITE_ROOT: '"http://localhost:8000/"'  
-}
--- Cameleon.Admin/src/utils/website.js
+++
@@ -1,6 +0,0 @@
-export default {
-  name: 'website',
-  getUrl (relativePath) {
-    return process.env.WEBSITE_ROOT + relativePath
-  }
-}
