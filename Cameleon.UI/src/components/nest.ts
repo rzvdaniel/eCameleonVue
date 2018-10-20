479e0e9bbdac3d5ad4b73dcd074445d58bbf8be9
Razvan Predescu <rzvdaniel@gmail.com>
2018-10-20 16:58:17 +0100

    Syntax experiment

--- Cameleon.UI/src/components/nest.ts
+++ Cameleon.UI/src/components/nest.ts
@@ -1,17 +1,28 @@
 import Vue from 'vue';
-import { Component } from 'vue-property-decorator';
+import { Component, Prop } from 'vue-property-decorator';
 import WithRender from './nest.html';
 
 @WithRender
 @Component({})
 export default class NestComponent extends Vue {
-  public data() {
-    return {
-      text: 'I\'m an alligator!',
-    };
+
+  // data
+  // private text: string = 'I\'m an aligator';
+  @Prop() private text!: string;
+
+  // computed
+  public get computed(): string {
+    return this.text.split('').reverse().join('');
   }
 
-  private baz() {
-    const c = 1;
+  // methods
+  public changeMessage() {
+    this.text = 'Good bye';
   }
+
+  // Lifecycle hooks
+  // created() { }
+  // mounted() { }
+  // updated() { }
+  // destroyed() { }
 }
